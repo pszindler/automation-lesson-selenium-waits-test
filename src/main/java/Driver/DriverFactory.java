6f524314087a7af00bf3132a670f6414be8693eb
Patryk <pszindler@sii.pl>
2022-09-22 11:54:17 +0200

    new type of switch

--- src/main/java/Driver/DriverFactory.java
+++ src/main/java/Driver/DriverFactory.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 
 public class DriverFactory {
     public WebDriver createInstance (String browserFromConfig) {
-
         BrowserTypes browser = BrowserTypes.valueOf(browserFromConfig.toUpperCase());
         return switch (browser) {
             case CHROME -> new ChromeDriverManager().createDriver();
