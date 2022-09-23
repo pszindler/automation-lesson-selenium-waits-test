15919d44e23a23067ede03679584dabfb9bc8eb4
Patryk <pszindler@sii.pl>
2022-09-23 11:04:14 +0200

    fixed problem with custom exception

--- src/main/java/Exceptions/BrowserNotSupportedException.java
+++ src/main/java/Exceptions/BrowserNotSupportedException.java
@@ -2,6 +2,6 @@ package Exceptions;
 
 public class BrowserNotSupportedException extends IllegalStateException {
     public BrowserNotSupportedException(String browser) {
-        super(String.format("Browser not supported: %s", browser));
+        super(String.format("Browser not supported: %s. Consider to change value to: chrome, firefox, edge, ie or safari", browser));
     }
 }
