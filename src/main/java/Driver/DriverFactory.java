01b4d22037e8caddde5ebb0452198b9fd3936b44
Patryk <pszindler@sii.pl>
2022-09-23 11:02:35 +0200

    fixed problem with custom exception

--- src/main/java/Driver/DriverFactory.java
+++ src/main/java/Driver/DriverFactory.java
@@ -5,15 +5,22 @@ import Exceptions.BrowserNotSupportedException;
 import org.openqa.selenium.WebDriver;
 
 public class DriverFactory {
-    public WebDriver createInstance () throws BrowserNotSupportedException {
-        String browserFromConfig = System.getProperty("browserInUse");
-        BrowserTypes browser = BrowserTypes.valueOf(browserFromConfig.toUpperCase());
-        return switch (browser) {
+    public WebDriver createInstance() {
+        return switch (getBrowserFromCfg()) {
             case CHROME -> new ChromeDriverManager().createDriver();
             case FIREFOX -> new FirefoxDriverManager().createDriver();
             case IE -> new IEDriverManager().createDriver();
             case SAFARI -> new SafariDriverManager().createDriver();
             case EDGE -> new EdgeDriverManager().createDriver();
         };
+    }
+
+    public BrowserTypes getBrowserFromCfg() {
+        String browser = System.getProperty("browserInUse").toUpperCase();
+        try {
+            return BrowserTypes.valueOf(browser);
+        } catch (IllegalArgumentException e) {
+            throw new BrowserNotSupportedException(browser);
+        }
     }
 }
--- src/main/java/Exceptions/BrowserNotSupportedException.java
+++ src/main/java/Exceptions/BrowserNotSupportedException.java
@@ -1,9 +1,7 @@
 package Exceptions;
 
-import Driver.BrowserTypes;
-
 public class BrowserNotSupportedException extends IllegalStateException {
-    public BrowserNotSupportedException(BrowserTypes browser) {
+    public BrowserNotSupportedException(String browser) {
         super(String.format("Browser not supported: %s", browser));
     }
 }
