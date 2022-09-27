5015bb967f4b893844a43094ae205749ad9f7634
Patryk <pszindler@sii.pl>
2022-09-27 11:14:07 +0200

    static pages added

---
+++ src/main/java/Pages/SummaryPage/ConfirmationPage.java
@@ -0,0 +1,19 @@
+package Pages.SummaryPage;
+
+import Pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ConfirmationPage extends BasePage {
+    public ConfirmationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".card-title.h1")
+    private WebElement titleConfirmed;
+
+    public String getConfirmationStatus() {
+        return titleConfirmed.getText();
+    }
+}
--- src/test/java/TestBase/Pages.java
+++ src/test/java/TestBase/Pages.java
@@ -1,12 +1,25 @@
 package TestBase;
 
+import Pages.ModalDialog.DialogPage;
+import Pages.ShoppingCart.ShoppingCartPage;
+import Pages.SummaryPage.AddressesPage;
+import Pages.SummaryPage.ConfirmationPage;
+import Pages.SummaryPage.PaymentPage;
 import org.junit.jupiter.api.BeforeEach;
 
 public class Pages extends TestBase {
-
+    public static DialogPage dialogPage;
+    public static ShoppingCartPage shoppingCartPage;
+    public static AddressesPage addressesPage;
+    public static ConfirmationPage confirmationPage;
+    public static PaymentPage paymentPage;
 
     @BeforeEach
     public void createInstances() {
-
+        dialogPage = new DialogPage(driver);
+        shoppingCartPage = new ShoppingCartPage(driver);
+        addressesPage = new AddressesPage(driver);
+        confirmationPage = new ConfirmationPage(driver);
+        paymentPage = new PaymentPage(driver);
     }
 }
