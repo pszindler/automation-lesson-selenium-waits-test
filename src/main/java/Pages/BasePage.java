c367838c33783dba7288ff8750922a77660ece92
Patryk <pszindler@sii.pl>
2022-09-27 09:11:11 +0200

    base code

---
+++ src/main/java/Pages/BasePage.java
@@ -0,0 +1,88 @@
+package Pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+
+public abstract class BasePage {
+    protected static final int WEB_ELEMENT_TIMEOUT = Integer.parseInt(System.getProperty("webElementTimeout"));
+    protected static final int WEB_ELEMENT_POOLING = Integer.parseInt(System.getProperty("webElementPooling"));
+    private static final Logger logger = LoggerFactory.getLogger(BasePage.class);
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
+    protected JavascriptExecutor jse;
+
+    public BasePage(WebDriver driver) {
+        initDrivers(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public BasePage(WebDriver driver, WebElement element) {
+        initDrivers(driver);
+        PageFactory.initElements(new DefaultElementLocatorFactory(element), this);
+    }
+
+    private void initDrivers(WebDriver driver) {
+        this.driver = driver;
+        this.jse = (JavascriptExecutor) driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(WEB_ELEMENT_TIMEOUT), Duration.ofMillis(WEB_ELEMENT_POOLING));
+        actions = new Actions(driver);
+        logger.debug("Created WebDriverWait with timeout: " + WEB_ELEMENT_TIMEOUT + "s and sleep: " + WEB_ELEMENT_POOLING + "ms");
+    }
+
+    protected void highLight(WebElement element, String color) {
+        jse.executeScript("arguments[0].style.border='3px solid " + color + '\'', element);
+    }
+
+    protected void highLight(WebElement element) {
+        highLight(element, "red");
+    }
+
+    public Dimension getViewPortSize() {
+        int width = Integer.parseInt(String.valueOf(jse.executeScript("return window.innerWidth")));
+        int height = Integer.parseInt(String.valueOf(jse.executeScript("return window.innerHeight")));
+
+        return new Dimension(width, height);
+    }
+
+    public boolean isPageLoaded() {
+        return isDOMLoaded() && isAjaxCompletedTasks();
+    }
+
+    public boolean isDOMLoaded() {
+        String state = jse.executeScript("return document.readyState").toString();
+        return state.equals("complete");
+    }
+
+    public boolean isAjaxCompletedTasks() {
+        String state = jse.executeScript("return jQuery.active").toString();
+        return state.equals("0");
+    }
+
+    public void waitForLoad() {
+        wait.until(driver -> isPageLoaded());
+    }
+
+    public void hoverOnElement(WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
+    protected String getSingleNodeText(WebElement element) {
+        String text = element.getText().trim();
+        List<WebElement> children = element.findElements(By.xpath("./*"));
+        for (WebElement child : children) {
+            text = text.replaceFirst(child.getText(), "").trim();
+        }
+        return text;
+    }
+}
--- src/test/java/TestBase/Pages.java
+++ src/test/java/TestBase/Pages.java
@@ -1,26 +1,12 @@
 package TestBase;
 
-import Pages.Category.ArtPage;
-import Pages.Category.Category;
-import Pages.HomePage.ProductsGridPage;
-import Pages.HomePage.TopMenu;
-import Pages.SearchPage.SearchResultPage;
 import org.junit.jupiter.api.BeforeEach;
 
 public class Pages extends TestBase {
 
-    public static ProductsGridPage productsGridPage;
-    public static TopMenu topMenu;
-    public static SearchResultPage searchResultPage;
-    public static Category category;
-    public static ArtPage artPage;
 
     @BeforeEach
     public void createInstances() {
-        productsGridPage = new ProductsGridPage(driver);
-        topMenu = new TopMenu(driver);
-        searchResultPage = new SearchResultPage(driver);
-        category = new Category(driver);
-        artPage = new ArtPage(driver);
+
     }
 }
