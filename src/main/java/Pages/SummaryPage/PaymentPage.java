253ac2b05da73ea10da9b9650170f8f2c5ab4a6d
Patryk <pszindler@sii.pl>
2022-09-27 11:06:23 +0200

    PaymentPage.java

---
+++ src/main/java/Pages/SummaryPage/PaymentPage.java
@@ -0,0 +1,25 @@
+package Pages.SummaryPage;
+
+import Pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PaymentPage extends BasePage {
+    public PaymentPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = "input#payment-option-1")
+    private WebElement paymentOptionByCheck;
+    @FindBy(css = "#checkout-payment-step [method] .ps-shown-by-js")
+    private WebElement agreeToTheTerms;
+    @FindBy(css = ".btn-primary.center-block")
+    private WebElement placeOrder;
+
+    public void proceedPayment() {
+        paymentOptionByCheck.click();
+        agreeToTheTerms.click();
+        placeOrder.click();
+    }
+}
